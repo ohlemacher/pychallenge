eb5f69f94eb6197fdecc3507cad505ffb096cfe1
David Ohlemacher <david.ohlemacher@verizon.com>
2015-11-28 19:31:16 -0500

    Solution 11. odd-even.

---
+++ 11_5808/odd_even_5808.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+11: 5808
+Given the image, which is obviously images mixed together somehow,
+find the hidden message.  The hint is 'odd and even'.
+"""
+
+import mahotas as mh
+import pylab
+
+def explore(fname):
+    """
+    Read the img into an ndarray named img.
+    Slice up the img.
+    Throw away odd rows and odd columns.
+    The answer is in what is left.
+    """
+    img = mh.imread(fname)
+    img_even = img[::2, ::2]
+    pylab.imshow(img_even)
+    pylab.show()
+
+
+if __name__ == '__main__':
+    FILE_NAME = 'cave.jpg'
+    explore(FILE_NAME)
